6611e2f0ef9f68813c441a3bb59c6e16a9b8d65c
Nicolas D <ndadevweb@gmail.com>
2025-07-30 13:31:18 +0200

    Magical Flex Recipes

---
+++ README.md
@@ -0,0 +1,10 @@
+# Using PHP-CS-Fixer
+
+composer require cs-fixer-shim
+
+To see the commands list
+./vendor/bin/php-cs-fixer
+
+To fix all files of the project
+./vendor/bin/php-cs-fixer fix
+
--- backend/src/Controller/MainController.php
+++ backend/src/Controller/MainController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController {
+class MainController
+{
     #[Route('/')]
-    public function homepage() {
+    public function homepage()
+    {
         return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
     }
 }
