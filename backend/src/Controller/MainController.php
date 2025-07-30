0085b039bd868ca74dd9a77d21c5ee322617ab6f
Nicolas D <ndadevweb@gmail.com>
2025-07-30 18:19:10 +0200

    Create a Service

--- backend/src/Controller/MainController.php
+++ backend/src/Controller/MainController.php
@@ -2,25 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/')]
-    public function homepage()
+    public function homepage(StarshipRepository $starshipRepository)
     {
-        $starshipCount = 457;
-        $myShip = [
-            'name' => 'USS LeafyCruiser (NCC-0001)',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'under construction',
-        ];
+        $ships = $starshipRepository->findAll();
+        $myShip = $ships[array_rand($ships)];
 
         return $this->render('main/homepage.html.twig', [
-            'numberOfStarships' => $starshipCount,
             'myShip' => $myShip,
+            'ships' => $ships,
         ]);
     }
 }
