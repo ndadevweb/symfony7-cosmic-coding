07f2eedd7bd2fb79c8eaa6459d06e4e29a4c9086
Nicolas D <ndadevweb@gmail.com>
2025-07-31 09:36:35 +0200

    Model Methods & Making the Design Dynamic

--- backend/src/Controller/MainController.php
+++ backend/src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage(StarshipRepository $starshipRepository)
     {
         $ships = $starshipRepository->findAll();
