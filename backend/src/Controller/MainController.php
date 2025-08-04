35c38722c35c87e2bd05bfa02e3bcbc549772497
Nicolas D <ndadevweb@gmail.com>
2025-08-04 12:19:45 +0200

    Variables environments and Twig function

--- backend/src/Controller/MainController.php
+++ backend/src/Controller/MainController.php
@@ -6,30 +6,19 @@ use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\Cache\ItemInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     public function homepage(
         StarshipRepository $starshipRepository,
-        HttpClientInterface $client,
-        CacheInterface $issLocationPool,
     ): Response {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
-        $issData = $issLocationPool->get('iss_location_data', function (ItemInterface $item) use ($client): array {
-            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-
-            return $response->toArray();
-        });
 
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
             'ships' => $ships,
-            'issData' => $issData,
         ]);
     }
 }
