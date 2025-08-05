f32292b35d96cebe5be97824b3f1faec4e829a06
Nicolas D <ndadevweb@gmail.com>
2025-08-05 20:00:51 +0200

    Auto inject entities

--- backend/src/Controller/StarshipController.php
+++ backend/src/Controller/StarshipController.php
@@ -2,22 +2,19 @@
 
 namespace App\Controller;
 
-use App\Repository\StarshipRepository;
+use App\Entity\Starship;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipController extends AbstractController
 {
-    #[Route('/starships/{id<\d+>}', name: 'app_starship_show', methods: ['GET'])]
-    public function show(int $id, StarshipRepository $repository): Response
-    {
-        $ship = $repository->find($id);
-
-        if (!$ship) {
-            throw $this->createNotFoundException('Starship not found');
-        }
-
+    #[Route('/starships/{slug}', name: 'app_starship_show')]
+    public function show(
+        #[MapEntity(mapping: ['slug' => 'slug'])]
+        Starship $ship,
+    ): Response {
         return $this->render('starship/show.html.twig', [
             'ship' => $ship,
         ]);
